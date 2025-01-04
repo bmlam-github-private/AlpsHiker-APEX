e961cbe4ac06d89c017dd6daad6f828c46159503
BMLam <mailto@bmlam.de>
2025-01-04 12:14:46 +1100

    get_ways_nodes.py got first result

--- get_ways_nodes.py
+++ get_ways_nodes.py
@@ -9,7 +9,11 @@ In addition we likely need a method to compute the positioning sequence of each 
 import overpy
 import sys
 import json
+import time 
+
 from dbx import _dbx , _errorExit
+
+overpass_conx = None 
 
 def extract_query (file_path):
 	# Open the file in read mode
@@ -23,11 +27,10 @@ def extract_query (file_path):
 	return "\n".join( lines )
 
 def query_ways ( query ):
-	# Initialize the Overpy API
-	api = overpy.Overpass()
+	global overpass_conx 
 
 	# Execute the query
-	result = api.query(query)
+	result = overpass_conx.query(query)
 	if len( result.ways ) == 0:
 		_errorExit( "No ways found in query result")
 	_dbx( "relations: %d" % len( result.relations) )
@@ -36,16 +39,30 @@ def query_ways ( query ):
 	#
 	return result 
 
+def get_way_nodes_coordinates( way ):
+	_dbx( f" {time.strftime('%X')}: getting nodes for id {way.id}")
+	nodes = way.get_nodes( resolve_missing= True )
+	str_list = []
+	for node in nodes: 
+		str_list.append( f"[{node.lon}, {node.lat}]")
+	return "[" + ",".join( str_list ) + "]"
+
 def extract_ways_meta( ways ):
+	ret_val = ""
 	for way in ways:
 		name = way.tags.get( "name" )
 		info = f"{way.id}"
 		if name != None: 
 			info += f" {name}"
-		_dbx( info )
+		#_dbx( info )
+		ret_val += "\n" + info 
+	return ret_val 
 
 # Main script logic
 if __name__ == "__main__":
+	# Initialize the Overpy overpass_conx
+	overpass_conx = overpy.Overpass()
+
 	# Ensure the file path is provided as a command-line argument
 	if len(sys.argv) != 2:
 		_dbx("Usage: python script.py <file_path>")
@@ -57,12 +74,21 @@ if __name__ == "__main__":
 	# Call the function to read the file and get the lines
 	try:
 		query = extract_query( file_path )
-		# Print the result
-		_dbx( query )
-		result = query_ways( query )
-		extract_ways_meta( result.ways)
 	except FileNotFoundError:
-		print(f"Error: The file '{file_path}' was not found.")
+		_errorExit(f"Error: The file '{file_path}' was not found.")
 	except Exception as e:
-		print(f"An error occurred: {e}")
+	#
+		_errorExit(f"An error occurred: {e}")
+	_dbx( query )
 
+	result = query_ways( query )
+	way_infos = extract_ways_meta( result.ways)
+	# print( way_infos )
+	line_str_list = []
+	for way in result.ways [ 0: 3 ]:
+		coord_str= get_way_nodes_coordinates( way )
+		_dbx( coord_str )
+		line_str_list.append( coord_str )
+	multi_line_str = '[' + "\n ,".join ( line_str_list ) + ']'
+	geojson_data = '{"type": "MultiLineString" \n , "coordinates": \n' + multi_line_str + ' }'
+	print( geojson_data )
